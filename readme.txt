754057553f78fbb70a79a50260de82f9bcbb64a3
zicohao <20887053@qq.com>
2018-01-18 16:22:35 +0800

    3rd commit test

--- readme.txt
+++ readme.txt
@@ -1,4 +1,4 @@
-Readme
+Readme file, i have no idea to write sth.
 
 by Hao
 Jan18,2018
---
+++ test.c
@@ -0,0 +1,19 @@
+I'm commit to github now!
+
+
+void main(void)
+{
+
+while(1)
+{
+
+}
+
+}
+
+
+Just for github study test only
+
+by hao
+
+Jan18,2018
